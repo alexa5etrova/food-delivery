83d95e54f166e1abc5c8807c031f0ad7fe72b4e1
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-04-22 21:50:38 +0300

    Apply http request

--- src/components/Meals/AvailableMeal.js
+++ src/components/Meals/AvailableMeal.js
@@ -1,36 +1,60 @@
 import Card from "../UI/Card";
 import styles from "./AvailableMeal.module.css";
 import Meal from "./Meal";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
 
 const AvailableMeal = (props) => {
-  const mealList = DUMMY_MEALS.map((item) => {
+  const [mealData, setMealData] = useState([]);
+  const [isLoading, setIsLoadind] = useState(true);
+  const [isError, setIsError] = useState();
+
+  useEffect(() => {
+    const fetchingMeal = async () => {
+      const response = await fetch(
+        "https://react-http-27168-default-rtdb.firebaseio.com/meal.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+      let meals = [];
+      for (let key in data) {
+        meals.push({
+          id: data[key],
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMealData(meals);
+      setIsLoadind(false);
+    };
+
+    fetchingMeal().catch((error) => {
+      setIsLoadind(false);
+      setIsError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p className={styles.loading}>Loading...</p>
+      </section>
+    );
+  }
+  if (isError) {
+    console.log(isError);
+    return (
+      <section>
+        <p className={styles.loading}>{isError}</p>
+      </section>
+    );
+  }
+
+  const mealList = mealData.map((item) => {
     return (
       <Meal
         key={item.id}
@@ -44,6 +68,7 @@ const AvailableMeal = (props) => {
   return (
     <section className={styles.meals}>
       <Card>
+        {isLoading && <p>Loading</p>}
         <ul>{mealList}</ul>
       </Card>
     </section>
